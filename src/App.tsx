ecad2dd7d6f27d50ddf8b41ec4aa90c18ea3e69e
Gentrit Uka <gentrit-uka@live.com>
2022-08-30 16:14:52 +0200

    updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,37 +1,35 @@
-import { useEffect, useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { useEffect, useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 type Quote = {
-  quote: string
-  author: string
-}
+  id: number;
+  quote: string;
+  author: string;
+};
 
 function App() {
-  const [quotes, setQuotes] = useState<Quote[]>([])
+  const [quotes, setQuotes] = useState<Quote[]>([]);
 
   useEffect(() => {
     fetch(`http://localhost:4000/quotes`)
-      .then(res => res.json())
-      .then(quotesFromServer => setQuotes(quotesFromServer))
-
-  }, [])
+      .then((res) => res.json())
+      .then((quotesFromServer) => setQuotes(quotesFromServer));
+  }, []);
 
   return (
     <div className="App">
-        <ul className='quote-list'>
+      <ul className="quote-list">
         <h1>Quotes</h1>
-          {quotes.map(quote => 
-          <li>
-            <div className='quote-text'>
-              <h4>"{quote.quote}"</h4>
-              <h2>- {quote.author}</h2>
-            </div>
+        {quotes.map((quote) => (
+          <li key={quote.id}>
+            <h4>"{quote.quote}"</h4>
+            <h2>- {quote.author}</h2>
           </li>
-          )}
-        </ul>
+        ))}
+      </ul>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
